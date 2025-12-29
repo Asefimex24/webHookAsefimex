04fe6bdd8cd54eaed40bbed5aafd0f3854073980
Ever Lopez <elmartinez@asefimex.com>
2025-12-29 13:34:02 -0600

    se ajusta hook para conexion usando .env

--- hook/webhook.php
+++ hook/webhook.php
@@ -1,12 +1,14 @@
 <?php
+require_once('getEnv.php');
+
 // Recibir datos de Twilio
 $from = $_POST['From'] ?? '';
 $body = $_POST['Body'] ?? '';
 $sid = $_POST['MessageSid'] ?? '';
 
 if (!empty($from) && !empty($body)) {
-    $dsn = "mysql:host=localhost;dbname=webhookasefimex;charset=utf8";
-    $pdo = new PDO($dsn, "root", "");
+    $dsn = "mysql:host=" . $_ENV['HOST'] . ";dbname=" . $_ENV['DB'] . ";charset=utf8";
+    $pdo = new PDO($dsn, $_ENV['USER'], $_ENV['PASSWORD']);
 
     $sql = "INSERT INTO mensajes_whatsapp (whatsapp_sid, remitente, mensaje) VALUES (?, ?, ?)";
     $stmt = $pdo->prepare($sql);
