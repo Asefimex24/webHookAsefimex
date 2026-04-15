f3bc9403ab1266c12d2cfef52cd9efc4164e3ed8
Ever Lopez <elmartinez@asefimex.com>
2026-04-15 11:38:39 -0600

    se ajusta visualización de contactos

--- js/getContactos.js
+++ js/getContactos.js
@@ -1,5 +1,5 @@
 function cargarMensajes() {
-    fetch('response/get_messages2.php')
+    fetch('response/getContactos.php')
     
         .then(response => response.text())
         .then(html => {
@@ -9,11 +9,12 @@ function cargarMensajes() {
                 tabla.innerHTML = html;
             }
         })
-        .catch(error => console.error('Error cargando mensajes:', error));
+        .then(() => console.log('Contactos cargados correctamente'))
+        .catch(error => console.error('Error cargando contactos:', error));
 }
 
 // Ejecutar cada 3 segundos
-setInterval(cargarMensajes, 3000);
+//setInterval(cargarMensajes, 3000);
 
 // Cargar al abrir la página por primera vez
 document.addEventListener('DOMContentLoaded', cargarMensajes);
--- response/getContactos.php
+++ response/getContactos.php
@@ -5,7 +5,7 @@ require_once '../getEnv.php';
 $dsn = "mysql:host=" . $_ENV['HOST'] . ";dbname=" . $_ENV['DB'] . ";charset=utf8mb4";
 $pdo = new PDO($dsn, $_ENV['USER'], $_ENV['PASSWORD']);
 
-$mensajes = $pdo->query("SELECT remitente, mensaje, fecha_recibido
+$mensajes = $pdo->query("SELECT remitente, fecha_recibido,mensaje
 FROM mensajes_whatsapp
 WHERE id IN (
     SELECT MAX(id)
@@ -16,20 +16,15 @@ ORDER BY fecha_recibido DESC LIMIT 30;")->fetchAll();
 
 foreach ($mensajes as $m):
 
-    if (mb_strlen($m['mensaje'], "UTF-8") > 30) {
-        $muestraMensaje = mb_substr($m['mensaje'], 0, 70, "UTF-8") . ' ...';
-    } else {
-        $muestraMensaje = $m['mensaje'];
-    }
 ?>
     <div class="contact-item p-3 d-flex align-items-center" data-phone="<?php echo $m['remitente']; ?>">
-        <img src="https://ui-avatars.com/api/?name=Juan+Perez" class="rounded-circle me-3" width="45">
+        <img src="https://ui-avatars.com/api/?name=Cliente+Asefimex" class="rounded-circle me-3" width="45">
         <div class="w-100">
             <div class="d-flex justify-content-between">
                 <h6 class="mb-0 text-truncate" style="max-width: 150px;"><?php echo $m['remitente']; ?></h6>
                 <small class="text-muted"><?php echo date("d-M-yy", strtotime($m['fecha_recibido'])); ?></small>
             </div>
-            <!-- <small class="text-muted text-truncate d-block" style="max-width: 200px;">Hola, ¿tienen disponibilidad?</small> -->
+             <small class="text-muted text-truncate d-block" style="max-width: 200px;"><?php echo $m['mensaje']; ?></small> 
         </div>
     </div>
 
