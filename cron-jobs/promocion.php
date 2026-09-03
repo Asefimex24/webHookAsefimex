a6ea3993b8d2b26bd3faa75d217265791f39ada2
Ever <ever@asefimex.com>
2026-09-03 09:56:29 -0600

    se agrega visualizacion de logs

--- cron-jobs/promocion.php
+++ cron-jobs/promocion.php
@@ -6,10 +6,7 @@ echo 'proceso ejecutado el: ' . date('Y-m-d H:i:s') . "\n";
 
 date_default_timezone_set("America/Mexico_City");
 
-//mostrar errores en tiempo de ejecucion
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 require_once __DIR__ . '/../getEnv.php';
 
